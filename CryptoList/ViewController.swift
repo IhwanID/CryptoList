6a75b77666923de6b93622f0010175cf74628ef1
ihwan <ihwan.id@icloud.com>
2022-02-11 20:34:17 +0700

    initialize project

---
+++ CryptoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoList
+//
+//  Created by Ihwan on 11/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
